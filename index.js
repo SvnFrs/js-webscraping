3d3857d012cf59a299399d2e834f7ec7b6254e59
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-07-02 21:38:51 +0700

    Navigate between all pages using the "Next" button and fetch the quotes on all the pages.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer";
-import { writeFile } from "fs";
+import { appendFile } from "fs";
 
 const getQuotes = async () => {
     const browser = await puppeteer.launch({
@@ -13,24 +13,32 @@ const getQuotes = async () => {
         waitUntil: "domcontentloaded",
     });
 
-    const allQuotes = await page.evaluate(() => {
-        const quotes = document.querySelectorAll(".quote");
-
-        return Array.from(quotes).map((quote) => {
-            const text = quote.querySelector(".text").innerHTML;
-            const author = quote.querySelector(".author").innerHTML;
-
-            return { text, author };
+    const run = async () => {
+        const allQuotes = await page.evaluate(() => {
+            const quotes = document.querySelectorAll(".quote");
+    
+            return Array.from(quotes).map((quote) => {
+                const text = quote.querySelector(".text").innerHTML;
+                const author = quote.querySelector(".author").innerHTML;
+                const tags = quote.querySelector('.tags')?.querySelector('.keywords')?.getAttribute('content');
+    
+                return { text, author, tags };
+            });
         });
-    });
+    
+        appendFile('quotes.json', JSON.stringify(allQuotes, null, '\t'), err => {
+            if (err || !allQuotes) {
+                console.log('error');
+            } else {
+                console.log('all good');
+            }
+        });
+    };
 
-    writeFile('data.json', JSON.stringify(allQuotes), err => {
-        if (err || !allQuotes) {
-            console.log('error');
-        } else {
-            console.log('all good');
-        }
-    });
+    while (await page.$('.pager > .next > a')) {
+        await page.click('.pager > .next > a');
+        run();
+    }
 
     await browser.close();
 };
